41f01ed96ac6a4a50c6e12c822040874e0ab9db3
Rowan Medhurst <rowanmedhurst@hotmail.com>
2018-05-26 22:40:38 +0100

    Update test.js
    Me and Bill Gates looked at this and we added a comment, Juju might remove it because she might think I did it by myself. But I made Bill Gates talk to her mum on the phone for 3 minutes. I'm a better programmer.

--- test.js
+++ test.js
@@ -72,4 +72,4 @@ console.log("Setting opacity to: "+window.opacity);
 window.brightness = 0.5;
 console.log("Setting brightness to: "+window.brightness);
 process.exitCode = 42;
-console.log("Process should now quit with return value "+process.exitCode);
+console.log("Process should now quit with return value "+process.exitCode);  // THIS CODE IS WORTH PRINTING OUT [bill gates told me juju so... i guess you should leave this comment in. but i told him before he said it. SO WHAT HE KNOWS BETTER THAN ME (i said this bill leave it in)]
